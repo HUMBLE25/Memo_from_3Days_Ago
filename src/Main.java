ecf61185f5341eb0565089a38843c104d46e90e1
humblei <wlsrua1007@gmail.com>
2024-11-19 00:17:11 +0900

    Image files loaded successfully

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,23 @@ import stage4.Stage4;
 import ending.Ending;
 import opening.Opening;
 
-public class Main {
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JFrame{
+    private ImageIcon imageIcon =new ImageIcon("images/opening/3일뒤메모.png");
+    JLabel imgLabel = new JLabel(imageIcon);
+    public Main(){
+        setTitle("Can cal images...??");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+        setSize(1440,1024);
+        c.add(imgLabel);
+        setVisible(true);
+    }
     public static void main(String[] args) {
+        new Main();
         System.out.println("Game Start!");
         System.out.println("test git, GO!GO!");
         System.out.println("keep going");
