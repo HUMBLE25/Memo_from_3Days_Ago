3447e415778b3563fe39913a41b9ccf42d95c0a1
humblei <wlsrua1007@gmail.com>
2024-11-26 09:06:05 +0900

    discovered a method to transition between scenes effectively

---
+++ src/ending/EndingData.java
@@ -0,0 +1,15 @@
+package ending;
+
+import common.SceneData;
+
+import javax.swing.*;
+
+public class EndingData {
+    public static SceneData[] getScenes(){
+        return new  SceneData[]{
+            new SceneData(null, null,
+                    null, null, new ImageIcon("images/stage2/학교앞장면.png"))
+
+        };
+    }
+}
